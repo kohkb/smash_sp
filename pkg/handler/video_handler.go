1a06c7f686713b60af54ded22bccb1d66f7ce275
kohkb <k.oh9.380@gmail.com>
2021-07-17 12:26:15 +0900

    Add video_handler.go

--- pkg/handler/video_handler.go
+++ pkg/handler/video_handler.go
@@ -1,1 +1,20 @@
 package handler
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/kohkb/smash_sp/pkg/gateway"
+)
+
+func GetVideos(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	client := gateway.NewYoutubeClient()
+	videos, err := client.SearchVideosByQuery(r.URL.Query().Get("q"))
+
+	if err != nil {
+		panic("runtime error")
+	}
+
+	json.NewEncoder(w).Encode(&videos)
+}
