eda78148cc61eab3e6a56da3fcc829886e485b19
kohkb <k.oh9.380@gmail.com>
2021-08-31 23:05:52 +0900

    Add axios

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,33 @@
 import Vue from 'vue'
 import App from './App.vue'
+import axios from 'axios'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  el: '#app',
+  data () {
+      return {
+          info: null,
+          loading: true,
+          errored: false
+      }
+  },
+  mounted() {
+      axios.get('http://localhost:8000/videos/favorites')
+      .then(response => {
+          this.info = response.data.bpi
+      })
+      .catch(error => {                
+          console.log(error)
+          this.errored = true
+      })
+      .finally(() => this.loading = false)
+  },
+
+  filters: {
+      currencydecimal (value) {
+          return value.toFixed(2)
+      }
+  }
+})
