ecd0ee567d34d66b8015cba427dba02c6c88e383
kohkb <k.oh9.380@gmail.com>
2021-09-08 23:10:10 +0900

    Remove helloworld

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -26,19 +26,9 @@
       </div>
 
       <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
     </v-app-bar>
 
-    <v-main>
-      <HelloWorld/>
+    <v-main>      
       <Video/>
     </v-main>
   </v-app>
