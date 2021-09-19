e78aac546d53f4440a600612ebbc101decef4197
kohkb <k.oh9.380@gmail.com>
2021-09-20 05:59:22 +0900

    adjust App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,39 +1,34 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Smash Logo"
-          class="shrink mr-2"
-          contain
-          src="/smash_logo.jpeg"
-          transition="scale-transition"
-          width="40"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-    </v-app-bar>
-
-    <v-main>      
-      <Video/>      
+    <div>
+      <v-app-bar
+        color="gray accent-4"
+        dark
+      >
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+        <v-toolbar-title>Smash Sp</v-toolbar-title>
+        <div class="flex-grow-1"></div>
+        <v-btn icon>
+          <v-icon>mdi-heart</v-icon>
+        </v-btn>
+        <v-btn icon>
+          <v-icon>mdi-magnify</v-icon>
+        </v-btn>
+      </v-app-bar>
+    </div>
+    <v-main>
+      <v-content>
+        <v-container>
+         <router-view></router-view> 
+        </v-container>
+      </v-content>            
     </v-main>
   </v-app>
 </template>
 
 <script>
-import Video from './components/Video';
-
 export default {
   name: 'App',
-
-  components: {    
-    Video
-  },
 
   data: () => ({
     //
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,10 +1,24 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
+import Video from './components/Video';
 import vuetify from './plugins/vuetify'
 
 Vue.config.productionTip = false
 
-new Vue({
+// TODO: Routerを別ファイルに移す
+Vue.use(VueRouter)
+
+const routes = [
+  {path: '/', component: Video}
+]
+
+const router = new VueRouter({
+  routes
+})
+
+new Vue({  
   vuetify,
-  render: h => h(App)
+  render: h => h(App),
+  router: router
 }).$mount('#app')
