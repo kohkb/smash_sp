9a7e8702ec8933ff023189cfd95d181463b15c48
kohkb <k.oh9.380@gmail.com>
2021-09-25 22:52:19 +0900

    Add removeFavorite

--- frontend/src/components/Video.vue
+++ frontend/src/components/Video.vue
@@ -9,7 +9,7 @@
                 <v-col cols=12 v-for="(video, key) in info" :key="key" sm=10 md=6 lg=6 xl=3 >
                     <v-card height=300 class="darken-4">
                         <iframe width="100%" height="100%" :src="video.VideoId | videoUrl" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>                                                                          
-                        <v-btn elevation="2" v-on:click="removeFavorite">お気に入り解除</v-btn>                                                
+                        <v-btn elevation="2" @click='removeFavorite(video.ID)'>お気に入り解除</v-btn>                                                                                             
                     </v-card>
                 </v-col>
             </v-row>              
@@ -42,9 +42,16 @@
       .finally(() => this.loading = false)
   },
   methods: {
-      removeFavorite: function() {
-          console.log("hello")
-        //   remove用のエンドポイントを作る        
+      removeFavorite: function(id) {        
+        axios.delete('/api/videos/favorites/' + id)
+        .then(response => {
+        console.log(response.data)
+      })
+      .catch(error => {                
+        console.log(error)
+        this.errored = true
+      })
+      .finally(() => this.loading = false)              
       }
   },
   filters: {
